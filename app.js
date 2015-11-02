3645b3b8a2ccc7f154b6c3775514b682588eef71
System Administrator <root@Israels-MacBook-Pro.local>
2015-11-02 00:44:37 +0000

    Checking Barcode Scanner Functionality

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ function onLoad() {
 var deviceReady = $(function () {
     // causes a sidebar to pop-up on click of menu button
     $(".button-collapse").sideNav();
-    
-    
+
+
 
     //This function will be used to send an Ajax call to a database
     function sendRequest(dataString) {
@@ -25,22 +25,21 @@ var deviceReady = $(function () {
 
     // An ajax call to save product
     var saveProduct = function () {
-     $("#save-product").on('click', function () {
+        $("#save-product").on('click', function () {
             var a = $("#product_name").val();
             var b = $("#product_price").val();
             var c = $("#product_quant").val();
             var d = $("#product_barcode").val();
-            var str = 'opt=1&product_name=' + a + '&product_price=' + b + '&product_quant=' +c+'&product_barcode='+d;
-           alert("Hello");
+            var str = 'opt=1&product_name=' + a + '&product_price=' + b + '&product_quant=' + c + '&product_barcode=' + d;
             $obj = sendRequest(str);
             var $toastContent = $obj.message;
             Materialize.toast($toastContent, 3000);
-        });    
+        });
     }
-    
+
     //An ajax call to delete a product from the database
-     var deleteProduct = function () {
-            $("ul").on('click','li .delete-product', function () {
+    var deleteProduct = function () {
+        $("ul").on('click', 'li .delete-product', function () {
             var id = $(this).prop("id");
             var str = 'opt=3&product_id=' + id;
             $obj = sendRequest(str);
@@ -48,14 +47,14 @@ var deviceReady = $(function () {
             Materialize.toast($toastContent, 3000);
             fetchProduct();
         });
-            
+
     }
-     
-      //An ajax call to edit a product in the database
-     var editModal = function () {
-            var id;
-         // This part populates the form
-            $("ul").on('click','li .edit-product', function () {
+
+    //An ajax call to edit a product in the database
+    var editModal = function () {
+        var id;
+        // This part populates the form
+        $("ul").on('click', 'li .edit-product', function () {
             id = $(this).prop("id");
             var str = 'opt=4&product_id=' + id;
             $obj = sendRequest(str);
@@ -66,57 +65,69 @@ var deviceReady = $(function () {
             $("#product_quant_1").val($obj.data[0].product_quantity);
             $("#product_barcode_1").val($obj.data[0].product_barcode);
         });
-         
-         // This part executes the edit function
-         $("#edit-product").click(function () {
+
+        // This part executes the edit function
+        $("#edit-product").click(function () {
             var a = $("#product_name_1").val();
             var b = $("#product_price_1").val();
             var c = $("#product_quant_1").val();
             var d = $("#product_barcode_1").val();
-            var str = 'opt=5&product_id='+id+'&product_name='+a+'&product_price='+b+'&product_quantity='+c+'&product_barcode='+d;
-            $obj = sendRequest(str);    
-            var $toastContent = $obj.message; 
+            var str = 'opt=5&product_id=' + id + '&product_name=' + a + '&product_price=' + b + '&product_quantity=' + c + '&product_barcode=' + d;
+            $obj = sendRequest(str);
+            var $toastContent = $obj.message;
             Materialize.toast($toastContent, 3000);
-            
+
         });
-            
+
     }
-    
-    
+
+
     //An Ajax call to fetch data from server
-    var fetchProduct = function() {
-        
+    var fetchProduct = function () {
+
         $obj = sendRequest("opt=2");
         var $toastContent = $obj.message;
         Materialize.toast($toastContent, 3000);
-        
-        
-            var data = $obj.data;
-           
-            var mid = "";
-            for (var i = 0; i < data.length; i++) {
-                mid = mid + '<li><div class="collapsible-header"><i class="fa fa-dot-circle-o"></i>'  +data[i].product_name + '<span class="price">&cent;' + data[i].product_price + '</span></div><div class="collapsible-body"><p>'+data[i].product_quantity+' are in stock</p><div class="control"><a class="btn-floating red delete-product" id="'+data[i].product_id+'"><i class="fa fa-trash"></i></a><a class="btn-floating yellow edit-product" href="#modal-edit" id="'+data[i].product_id+'"><i class="fa fa-edit"></i></a></div></div></li>';
-            }
-          
-            $("#listSection").html(mid);
-            list_control();
-        
-       
-        
+
+
+        var data = $obj.data;
+
+        var mid = "";
+        for (var i = 0; i < data.length; i++) {
+            mid = mid + '<li><div class="collapsible-header"><i class="fa fa-dot-circle-o"></i>' + data[i].product_name + '<span class="price">&cent;' + data[i].product_price + '</span></div><div class="collapsible-body"><p>' + data[i].product_quantity + ' are in stock</p><div class="control"><a class="btn-floating red delete-product" id="' + data[i].product_id + '"><i class="fa fa-trash"></i></a><a class="btn-floating yellow edit-product" href="#modal-edit" id="' + data[i].product_id + '"><i class="fa fa-edit"></i></a></div></div></li>';
+        }
+
+        $("#listSection").html(mid);
+        list_control();
     }
+        
+    
+        // The barcode of the product is scanned and form is filled
+        var makePurchase = function () {
+            $("#scan-btn-2").click(function () {
+                cordova.plugins.barcodeScanner.scan(
+                    function (result) {
+                        
+                        
+                    },
+                    function (error) {
+    
+                    }
+                );
+            });
+        }
 
-                    
 
+        //A function that triggers the barcode scanner to append to the edit form
     var barcode = function () {
-        $("#scan-btn").click(function () {
-            alert("hi");
+        $("#scan-btn, #scan-btn-1").click(function () {
             cordova.plugins.barcodeScanner.scan(
                 function (result) {
                     $("#product_barcode_1").val(result.text);
-                    $("#product_barcode_1").val(result.text);
+                    $("#product_barcode").val(result.text);
                 },
                 function (error) {
-                    $("#resultInfo").val("Scanning failed: " + error);
+
                 }
             );
         });
@@ -129,31 +140,43 @@ var deviceReady = $(function () {
             Materialize.toast('Add a product here', 2000);
         }, // Callback for Modal open
         complete: function () {
-            fetchProduct();
+                fetchProduct();
             } // Callback for Modal close
     });
-    
-    // A series of functions that are bound to each list, an alternative for method delegation
-    var list_control = function(){
-        $('.collapsible').collapsible({
-            accordion : false // setting true changes to collapsible
-            });
-            // this function triggers the modal at the bottom of the screen to add data
-    $('.edit-product').leanModal({
-        ready: function () {
-            Materialize.toast('Edit a product here', 2000); 
-        }, // Callback for Modal open
-        complete: function () {
-            fetchProduct();
-            } // Callback for Modal close
-    });
-    }
-  
-    
-    
+
    
+        $("#purchase").leanModal({
+            ready: function () {
+                Materialize.toast('Edit a product here', 2000);
+            }, // Callback for Modal open
+            complete: function () {
+                    fetchProduct();
+                } // Callback for Modal close
+        });
+    
 
 
+    // A series of functions that are bound to each list, an alternative for method delegation
+    var list_control = function () {
+        $('.collapsible').collapsible({
+            accordion: false // setting true changes to collapsible
+        });
+        // this function triggers the modal at the bottom of the screen to add data
+        $('.edit-product').leanModal({
+            ready: function () {
+                Materialize.toast('Edit a product here', 2000);
+            }, // Callback for Modal open
+            complete: function () {
+                    fetchProduct();
+                } // Callback for Modal close
+        });
+    }
+
+
+
+
+
+    makePurchase();
     //A function that populates the table
     fetchProduct();
     //executes a barcode reader on phones
@@ -163,6 +186,7 @@ var deviceReady = $(function () {
     //deletes a product based on the id
     deleteProduct();
     editModal();
+
 
 
 
